d214d0bf88d2674dbaf849510ccf93765ccad2e2
Duane Hutchins <duane.hutchins@bouncex.com>
2022-03-10 15:16:05 -0500

    Updated core-js@^3.4

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('core-js/fn/array/includes');
-require('core-js/fn/object/values');
+require('core-js/actual/array/includes');
+require('core-js/actual/object/values');
 var DOMException = require('domexception');
 var Typeson = require('typeson');
 var structuredCloningThrowing = require('typeson-registry/dist/presets/structured-cloning-throwing');
 
 // http://stackoverflow.com/a/33268326/786644 - works in browser, worker, and Node.js
-var globalVar = typeof window !== 'undefined' ? window : 
+var globalVar = typeof window !== 'undefined' ? window :
    typeof WorkerGlobalScope !== 'undefined' ? self :
    typeof global !== 'undefined' ? global :
    Function('return this;')();
